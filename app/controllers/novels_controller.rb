48bdb8b1983039c4d9a4d730eb9610021913cf14
Andrew Harrison <a.git14@uio9.org>
2016-03-26 14:42:02 +0100

    Novels controller with index and show.
    > rails generate controller Novels index show

---
+++ app/controllers/novels_controller.rb
@@ -0,0 +1,7 @@
+class NovelsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'novels/index'
+
+  get 'novels/show'
+
   resources :authors
 
 end
