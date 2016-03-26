03b8b47d4ade2c18da04190d7e31ab79d0a1ae42
Andrew Harrison <a.git14@uio9.org>
2016-03-26 19:35:18 +0100

    Genres Show and Index views.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,9 @@
 class GenresController < ApplicationController
   def index
+    @genres = Genre.all.sort_by { |genre| genre.novels.size }.reverse
   end
 
   def show
+     @genre = Genre.find( params[:id] )
   end
 end
