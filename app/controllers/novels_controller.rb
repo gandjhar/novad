aa98e6c79d5490ef4d0dfee6e79f9dccaae577bd
Andrew Harrison <a.git14@uio9.org>
2016-03-27 08:58:05 +0200

    New Novel.

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -6,4 +6,27 @@ class NovelsController < ApplicationController
   def show
     @novel = Novel.find( params[:id] )
   end
+
+  def new
+     @novel = Novel.new
+     @novel.author_id = params[:author_id]
+  end
+
+  def create
+     novel = Novel.new( novel_params )
+
+     if novel.save
+        redirect_to author_path( novel.author_id )
+     else
+        render new_novel_path
+     end
+  end
+
+private
+
+  def novel_params
+    params.require( :novel ).permit( :title, :year, :cover, :plot, :author_id, genre_ids: [] )
+  end
+
+
 end
